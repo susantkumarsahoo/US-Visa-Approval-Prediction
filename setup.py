8b79281f5228c3095d6d1bc3c0183775ee1b7ae0
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-08-24 02:17:18 +0530

    file uplode

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="susantkumar",
+    author_email="susantkumar.sks96@gmail.com",
+    packages=find_packages()
+)
+
+
